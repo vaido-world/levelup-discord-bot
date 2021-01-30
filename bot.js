47ce79cf3b4485bb25957a9cc3e785d7f2b50649
Feldwor <BoQsc@users.noreply.github.com>
2021-01-30 18:39:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,9 +28,9 @@ client.on('message', message => {
        if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
        // Crashes/shutdowns the bot.
        //message.member.setNickname(message.content.replace('changeNick ', ''));
-         var nick = message.content.split(" ").slice(1).join(" ");
-         message.member.setNickname(nick);
-         message.guild.members.get(message.author.id).setNickname("asd");
+         //var nick = message.content.split(" ").slice(1).join(" ");
+         //message.member.setNickname(nick);
+         //message.guild.members.get(message.author.id).setNickname("asd");
          message.channel.send('Tried to change username..');
     }
    
