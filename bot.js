7b93946199fb4f7f1e52165d5d73dd2dbeeeeb0d
Feldwor <BoQsc@users.noreply.github.com>
2021-05-17 21:49:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,10 +17,10 @@ const client = new Discord.Client();
       console.log('Bot is now connected')
         client.channels.find(channel => channel.name === 'bot').send("Hello there! ⚐"); // for discord v11 
         client.channels.find(channel => channel.name === 'bot').send("┠To edit this bot: <https://github.com/vaido-world/levelup-discord-bot/edit/main/bot.js> \
-        \n┠Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot>"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("┠The bot is hosted on Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github>"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("┠The bot is is about to use MongoDB database: <https://www.mongodb.com/>"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("┠Discord Settings for this Bot: <https://discord.com/developers/applications/804719647601459250/bot>"); // for discord v11
+        \n┠Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot> \
+        \n┠The bot is hosted on Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github> \
+        \n┠The bot is is about to use MongoDB database: <https://www.mongodb.com/> \
+        \n┠Discord Settings for this Bot: <https://discord.com/developers/applications/804719647601459250/bot>"); 
         client.channels.find(channel => channel.name === 'bot').send("┠Debugging logs for this Bot: <https://dashboard.heroku.com/apps/levelup-discord-bot/logs> \n[Some more Diagnostics, like server/client latency here?]"); // for discord v11 
      //client.channels.cache.find(channel => channel.name === 'bot1').send("Hello there!"); // for discord v12
     });
