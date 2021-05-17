b9a32e10e83bdb1b8858e184f961b78236bf25e6
Feldwor <BoQsc@users.noreply.github.com>
2021-05-17 21:10:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,13 +15,13 @@ const client = new Discord.Client();
     //https://stackoverflow.com/questions/60665866/discord-js-send-message-to-a-specific-channel-using-a-bot/60666036#60666036
     client.on('ready', () => {
       console.log('Bot is now connected')
-        client.channels.find(channel => channel.name === 'bot').send("Hello there!"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("To edit this bot: <https://github.com/vaido-world/levelup-discord-bot/edit/main/bot.js>"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot>"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("The bot is hosted on Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github>"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("The bot is is about to use MongoDB database: <https://www.mongodb.com/>"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("Discord Settings for this Bot: <https://discord.com/developers/applications/804719647601459250/bot>"); // for discord v11
-        client.channels.find(channel => channel.name === 'bot').send("Debugging logs for this Bot: <https://dashboard.heroku.com/apps/levelup-discord-bot/logs> \n[Some more Diagnostics, like server/client latency here?]"); // for discord v11 
+        client.channels.find(channel => channel.name === 'bot').send("Hello there! ⚐"); // for discord v11 
+        client.channels.find(channel => channel.name === 'bot').send("┠To edit this bot: <https://github.com/vaido-world/levelup-discord-bot/edit/main/bot.js>"); // for discord v11 
+        client.channels.find(channel => channel.name === 'bot').send("┠Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot>"); // for discord v11 
+        client.channels.find(channel => channel.name === 'bot').send("┠The bot is hosted on Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github>"); // for discord v11 
+        client.channels.find(channel => channel.name === 'bot').send("┠The bot is is about to use MongoDB database: <https://www.mongodb.com/>"); // for discord v11 
+        client.channels.find(channel => channel.name === 'bot').send("┠Discord Settings for this Bot: <https://discord.com/developers/applications/804719647601459250/bot>"); // for discord v11
+        client.channels.find(channel => channel.name === 'bot').send("┠Debugging logs for this Bot: <https://dashboard.heroku.com/apps/levelup-discord-bot/logs> \n[Some more Diagnostics, like server/client latency here?]"); // for discord v11 
      //client.channels.cache.find(channel => channel.name === 'bot1').send("Hello there!"); // for discord v12
     });
 
