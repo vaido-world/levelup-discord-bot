7bcb6ee1ef07ac31e7a7be38849bed89c1d18962
Feldwor <BoQsc@users.noreply.github.com>
2021-05-18 09:50:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const client = new Discord.Client();
         \n> ┠Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github> \
         \n> ┠MongoDB database: <https://www.mongodb.com/> \
         \n> ┠Discord Settings for this Bot: <https://discord.com/developers/applications/804719647601459250/bot>"); 
-        client.channels.find(channel => channel.name === 'bot').send("┠Debugging logs for this Bot: <https://dashboard.heroku.com/apps/levelup-discord-bot/logs> \n[Some more Diagnostics, like server/client latency here?]"); // for discord v11 
+        client.channels.find(channel => channel.name === 'bot').send("Debugging logs for this Bot: <https://dashboard.heroku.com/apps/levelup-discord-bot/logs> \n[Some more Diagnostics, like server/client latency here?]"); // for discord v11 
      //client.channels.cache.find(channel => channel.name === 'bot1').send("Hello there!"); // for discord v12
     });
 
