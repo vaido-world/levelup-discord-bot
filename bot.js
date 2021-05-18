63fe739946375a29751ebebdd98740bc24726b7a
Feldwor <BoQsc@users.noreply.github.com>
2021-05-18 09:44:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ const client = new Discord.Client();
       console.log('Bot is now connected')
         client.channels.find(channel => channel.name === 'bot').send("Hello there! ⚐ The bot is hosted on Heroku and will use MongoDB database"); // for discord v11 
         client.channels.find(channel => channel.name === 'bot').send("┠To edit this bot: <https://github.com/vaido-world/levelup-discord-bot/edit/main/bot.js> \
-        \n┠Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot> \
+        \n> ┠Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot> \
         \n┠Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github> \
         \n┠MongoDB database: <https://www.mongodb.com/> \
         \n┠Discord Settings for this Bot: <https://discord.com/developers/applications/804719647601459250/bot>"); 
