a9e2d60298f3d6b82af521990ab4f25c3291910b
Feldwor <BoQsc@users.noreply.github.com>
2021-05-18 10:26:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,10 +16,10 @@ const client = new Discord.Client();
     client.on('ready', () => {
       console.log('Bot is now connected')
         client.channels.find(channel => channel.name === 'bot').send("Hello there! ⚐ The bot is hosted on Heroku and will use MongoDB database"); // for discord v11 
-        client.channels.find(channel => channel.name === 'bot').send("To edit this bot: <https://github.com/vaido-world/levelup-discord-bot/edit/main/bot.js> 
-        client.channels.find(channel => channel.name === 'bot').send("> 📚 Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot> 
-        client.channels.find(channel => channel.name === 'bot').send("> 🗄 Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github> 
-        client.channels.find(channel => channel.name === 'bot').send("> 🧵 MongoDB database: <https://www.mongodb.com/> 
+        client.channels.find(channel => channel.name === 'bot').send("To edit this bot: <https://github.com/vaido-world/levelup-discord-bot/edit/main/bot.js>");
+        client.channels.find(channel => channel.name === 'bot').send("> 📚 Readme for this bot: <https://github.com/vaido-world/levelup-discord-bot>"); 
+        client.channels.find(channel => channel.name === 'bot').send("> 🗄 Heroku, Control Panel: <https://dashboard.heroku.com/apps/levelup-discord-bot/deploy/github>"); 
+        client.channels.find(channel => channel.name === 'bot').send("> 🧵 MongoDB database: <https://www.mongodb.com/>"); 
         client.channels.find(channel => channel.name === 'bot').send("> 📓 Discord Settings for this Bot: <https://discord.com/developers/applications/804719647601459250/bot>"); 
         client.channels.find(channel => channel.name === 'bot').send("Debugging logs for this Bot: <https://dashboard.heroku.com/apps/levelup-discord-bot/logs> \n[Some more Diagnostics, like server/client latency here?]"); // for discord v11 
      //client.channels.cache.find(channel => channel.name === 'bot1').send("Hello there!"); // for discord v12
