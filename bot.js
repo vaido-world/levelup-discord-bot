7c33937d948937da7d6a10485b4cdb2a8432dc58
Feldwor <BoQsc@users.noreply.github.com>
2021-05-19 22:19:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,16 +116,9 @@ console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client
   
 // -----------------------------Activity-----------------------------
 // Set the bot's presence (activity and status)
-client.on("ready", () => {
-    client.user.setPresence({
-        game: { 
-            name: 'my code',
-            type: 'WATCHING'
-        },
-        status: 'online'
-    })
+client.user.setActivity('yourGameNameHere', { type: 'PLAYING' });
  
-     console.log(`Hi, ${client.user.username} trying to set Bot Presence and Bot Status!`);
+     console.log(`${client.user.username} trying to set Bot Presence and Bot Status!`);
 }) // -----------------------------ActivityEND-----------------------------
 
 
