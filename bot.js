2bfc8e0318497b79e266697a8024c532e584a8ec
Feldwor <BoQsc@users.noreply.github.com>
2021-05-19 21:51:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client
  client.user.setPresence({
         game: {
             name: 'Use f!help',
-            type: "Watching",
+            type: "Playing",
             url: "https://discordapp.com/"
         }
     });
