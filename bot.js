6680ae863561a42e775ca7bc5183fc4f7c3ac163
Feldwor <BoQsc@users.noreply.github.com>
2021-05-19 23:25:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,7 +117,7 @@ console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client
 // -----------------------------Activity-----------------------------
 // Set the bot's presence (activity and status) 👈☕ ⇢🖥️🔖
 client.on('ready', () => {
-client.user.setActivity('💬: botas! ', { type: 'LISTENING' });
+client.user.setActivity('💬⇢ botas! ', { type: 'LISTENING' });
  
      console.log(`${client.user.username} trying to set Bot Presence and Bot Status!`);
 }) // -----------------------------ActivityEND-----------------------------
